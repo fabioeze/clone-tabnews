3c4604addde3c83bf1fb80a8e5dca95b37d17767
Fábio Deves <109983248+fabioeze@users.noreply.github.com>
2025-03-21 00:18:34 +0000

    style

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return <main>
         <h1>o vicio em pokemon, entenda</h1>
-        <img src="/duda.jpeg" style="width:100vw;"/>
+        <img src="/duda.jpeg" style={{ maxWidth: "100%", height: "auto" }} />
+
     </main>
 }
 
