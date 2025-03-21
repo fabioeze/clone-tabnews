856f7a575b0f9c8eac6047920a3a4eb449a4571c
Fábio Deves <109983248+fabioeze@users.noreply.github.com>
2025-03-21 00:15:29 +0000

    adiciona imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <><h1>o vicio em pokemon, entenda</h1><img src="/duda.jpeg"/></>
+    return <main style="width=100vw"><h1>o vicio em pokemon, entenda</h1>
+    <img src="/duda.jpeg"/>
+    </main>
 }
 
 export default Home;
