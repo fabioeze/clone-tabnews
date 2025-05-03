ca4091d18ad40cb7beb6cdbc64d157b14dbdcc86
Fábio Deves <109983248+fabioeze@users.noreply.github.com>
2025-05-03 21:50:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-    return <main>
-        <h1>o vicio em pokemon, entenda</h1>
-        <img src="/duda.jpeg" style={{ maxWidth: "100%", height: "auto" }} />
-
+  return (
+    <main>
+      <h1>o vicio em pokemon, entenda</h1>
+      <img src="/duda.jpeg" style={{ maxWidth: "100%", height: "auto" }} />
     </main>
+  );
 }
 
 export default Home;
