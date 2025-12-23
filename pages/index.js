7ac48f45967501fcffc4694583197577a6ce3ee2
Fábio Deves <109983248+fabioeze@users.noreply.github.com>
2025-12-23 12:13:39 +0000

    handles database SSL connection

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <main>
       <h1>o vicio em pokemon, entenda</h1>
-      <img src="/duda.jpeg" style={{ maxWidth: "100%", height: "auto" }} />
+      {/*<img src="/duda.jpeg" style={{ maxWidth: "100%", height: "auto" }} />*/}
     </main>
   );
 }
